514049d88082ac0a5dbfbd23bb45c877c8df6c40
Aggelosk <akermezo@yahoo.com>
2025-12-25 17:44:18 +0200

    offercard improvements

--- components/home/offer-card.tsx
+++ components/home/offer-card.tsx
@@ -5,36 +5,46 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import { ShieldCheck, CreditCard, Percent } from "lucide-react";
 
 type Offer = {
   title: string;
   description: string;
+  icon: React.ReactNode;
 };
 
 const offers: Offer[] = [
-  { title: "Extended Guarantee", description: "5 years on all watches" },
+  {
+    title: "Extended Guarantee",
+    description: "5 years on all watches",
+    icon: <ShieldCheck size={35} />,
+  },
   {
     title: "Credit Card Payment",
     description: "Flexible installments available",
+    icon: <CreditCard size={35} />,
   },
   {
     title: "20% Off Winter Offer",
     description: "Special offer on select models",
+    icon: <Percent size={35} />,
   },
 ];
-
 const OfferCard = () => {
   return (
     <section className="bg-black items-center p-10">
-      <div className="flex justify-center gap-8">
+      <div className="flex justify-center items-center gap-8 flex-wrap">
         {offers.map((offer, index) => (
           <Card
             key={index}
-            className="bg-black border-white text-center text-white hover:scale-105 transition-transform duration-300"
+            className="w-70 h-60 bg-black border-white text-center text-white hover:scale-105 transition-transform duration-300"
           >
             <CardHeader>
               <CardTitle className="text-xl">{offer.title}</CardTitle>
             </CardHeader>
+            <div className="text-orange-500 flex items-center justify-center">
+              {offer.icon}
+            </div>
             <CardContent>
               <CardDescription>{offer.description}</CardDescription>
             </CardContent>
