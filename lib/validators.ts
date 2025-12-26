626c7205ad38d90e141f331b03fcfa2f57a7f47a
Aggelosk <akermezo@yahoo.com>
2025-12-26 10:43:49 +0200

    validators file

---
+++ lib/validators.ts
@@ -0,0 +1,126 @@
+import { z } from "zod";
+import { PAYMENT_METHODS } from "./constants";
+
+// Schema for inserting products
+export const insertProductSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  slug: z.string().min(3, "Slug must be at least 3 characters"),
+  category: z.string().min(3, "Category must be at least 3 characters"),
+  brand: z.string().min(3, "Brand must be at least 3 characters"),
+  description: z.string().min(3, "Description must be at least 3 characters"),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, "Product must have at least one image"),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+  price: z.number().nonnegative("Price must be a positive number"),
+});
+
+// Schema for updating products
+export const updateProductSchema = insertProductSchema.extend({
+  id: z.string().min(1, "Id is required"),
+});
+
+// Schema for signing users in
+export const signInFormSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+// Schema for signing up a user
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["ConfirmPassword"],
+  });
+
+// Cart Schemas
+export const cartItemsSchema = z.object({
+  productId: z.string().min(1, "Product is required"),
+  name: z.string().min(1, "Name is required"),
+  slug: z.string().min(1, "Slug is required"),
+  qty: z.number().int().nonnegative("Quantity must be a positive number"),
+  image: z.string().min(1, "Image is required"),
+  price: z.number().nonnegative("Price must be a positive number"),
+});
+
+export const insertCartSchema = z.object({
+  items: z.array(cartItemsSchema),
+  itemsPrice: z.number().nonnegative("Items price must be a positive number"),
+  totalPrice: z.number().nonnegative("Total price must be a positive number"),
+  shippingPrice: z
+    .number()
+    .nonnegative("Shipping price must be a positive number"),
+  taxPrice: z.number().nonnegative("Tax price must be a positive number"),
+  sessionCartId: z.string().min(1, "Session cart ID is required"),
+  userId: z.string().optional().nullable(),
+});
+
+// Schema for the shipping address
+export const shippingAddressSchema = z.object({
+  fullName: z.string().min(3, "Name must be at least 3 characters"),
+  streetAddress: z.string().min(3, "Address must be at least 3 characters"),
+  city: z.string().min(3, "City must be at least 3 characters"),
+  postalCode: z.string().min(3, "Postal code must be at least 3 characters"),
+  country: z.string().min(3, "Country must be at least 3 characters"),
+  lat: z.number().optional(),
+  lng: z.number().optional(),
+});
+
+// Schema for payment method
+export const paymentMethodSchema = z
+  .object({
+    type: z.string().min(1, "Payment method is required"),
+  })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ["type"],
+    message: "Invalid payment method",
+  });
+
+// Schema for inserting order
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, "User is required"),
+  itemsPrice: z.number().nonnegative("Items price must be positive"),
+  shippingPrice: z.number().nonnegative("Shipping price must be positive"),
+  taxPrice: z.number().nonnegative("Tax price must be positive"),
+  totalPrice: z.number().nonnegative("Total price must be positive"),
+  paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+    message: "Invalid payment method",
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+// Schema for inserting an order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  name: z.string(),
+  price: z.number().nonnegative("Price must be positive"),
+  qty: z.number().int().nonnegative("Quantity must be positive"),
+});
+
+// Schema for the PayPal paymentResult
+export const paymentResultSchema = z.object({
+  id: z.string(),
+  status: z.string(),
+  email_address: z.string(),
+  pricePaid: z.string(),
+});
+
+// Schema for updating user profile
+export const updateProfileSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().min(3, "Email must be at least 3 characters"),
+});
+
+// Schema to update users
+export const updateUserSchema = updateProfileSchema.extend({
+  id: z.string().min(1, "ID is required"),
+  role: z.string().min(1, "Role is required"),
+});
