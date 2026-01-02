10bb27464ed1aeb0a4f3342d094ec9390610a89c
Aggelosk <akermezo@yahoo.com>
2026-01-02 11:24:03 +0200

    convert objects to string

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -134,13 +134,13 @@ export async function getMyCart() {
   if (!cart) return undefined;
 
   // Convert decimals and return
-  return convertToPlainObject({
+return convertToPlainObject({
     ...cart,
     items: cart.items as CartItem[],
-    itemsPrice: Number(cart.itemsPrice),
-    totalPrice: Number(cart.totalPrice),
-    shippingPrice: Number(cart.shippingPrice),
-    taxPrice: Number(cart.taxPrice),
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
   });
 }
 
