65e5b980f41fd1dd5c062fa092927b0b31b912d9
Aggelosk <akermezo@yahoo.com>
2026-01-11 16:09:00 +0200

    add home icons into admin and user layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,7 @@
-import { APP_NAME } from "@/lib/constants";
 import { Toaster } from "react-hot-toast";
-import Image from "next/image";
 import Link from "next/link";
 import MainNav from "@/app/admin/main-nav";
+import { House } from "lucide-react";
 
 export default function AdminLayout({
   children,
@@ -16,12 +15,7 @@ export default function AdminLayout({
         <div className="border-b w-full mx-auto">
           <div className="flex items-center h-16 px-4">
             <Link href="/" className="w-22">
-              <Image
-                src="/images/logo.svg"
-                height={48}
-                width={48}
-                alt={APP_NAME}
-              />
+              <House size={50} color="#ec6104" />
             </Link>
             <MainNav className="mx-6" />
             <div className="ml-auto items-center flex space-x-4"></div>
--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,7 +1,6 @@
-import { APP_NAME } from "@/lib/constants";
 import { Toaster } from "react-hot-toast";
-import Image from "next/image";
 import Link from "next/link";
+import { House } from "lucide-react";
 import MainNav from "@/app/user/main-nav";
 
 export default function UserLayout({
@@ -11,17 +10,12 @@ export default function UserLayout({
 }>) {
   return (
     <>
-      <div className="flex flex-col bg-black text-white h-screen w-full">
+      <div className="flex flex-col h-screen w-full">
         <Toaster position="bottom-right" />
         <div className="border-b w-full mx-auto">
           <div className="flex items-center h-16 px-4">
             <Link href="/" className="w-22">
-              <Image
-                src="/public/images/logo.svg"
-                height={48}
-                width={48}
-                alt={APP_NAME}
-              />
+              <House size={50} color="#ec6104" />
             </Link>
             <MainNav />
             <div className="ml-auto items-center flex space-x-4"></div>
